53e31e5c8d45d7439921d405fc1beb287cdfac4b
Azizul Tareq <azizul.tareq@northsouth.edu>
2021-03-12 16:55:41 +0600

    header and footer created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import react from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+
 function App() {
   return (
-    <div className="App">
-      proshop
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+
+      <Footer />
+    </>
   );
 }
 
