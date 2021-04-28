ae628c5fee1372e5183d6e2e5434e1501f7e4329
Azizul Tareq <azizul.tareq@northsouth.edu>
2021-04-28 22:42:33 +0600

    homepage modified, data comming from redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,38 @@
 import React, { useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions.js'
 
 const HomeScreen = () => {
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
 
     return (
         <>
             <h1>All Products</h1>
-            <Row>
+
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h3>{error}</h3>
+            ) : (
+                <Row>
                 {products.map(product => (
                     <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
                         <Product product={product} />
                     </Col>
                 ))}
             </Row>
+
+            )}
+            
 
             
         </>
