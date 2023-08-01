43a37284bbe70591d2a59fbddcb7bfdbeb7bec6f
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2023-08-01 21:03:44 +0600

    prepared to deploy to render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 import path from 'path'
 import express from 'express'
-import cors from 'cors'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
@@ -17,7 +16,6 @@ dotenv.config()
 connectDB()
 
 const app = express()
-app.use(cors())
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -37,17 +35,17 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   )
-// } else {
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
   app.get('/', (req, res) => {
     res.send('API is running....')
   })
-// }
+}
 
 app.use(notFound)
 app.use(errorHandler)
